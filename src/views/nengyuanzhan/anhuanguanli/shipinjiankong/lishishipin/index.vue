55a059398eaba8ab8750a0a376a28831cd9b9203
fuqianglu <lufuqiang1225@163.com>
2026-08-17 20:46:07 +0800

    历史视频优化

--- src/views/nengyuanzhan/anhuanguanli/shipinjiankong/lishishipin/index.vue
+++ src/views/nengyuanzhan/anhuanguanli/shipinjiankong/lishishipin/index.vue
@@ -243,7 +243,7 @@
 <style lang="less" scoped>
   .app-container {
     box-sizing: border-box;
-    height: calc(100vh - 104px);
+    height: calc(100vh - 110px);
     padding: 10px;
     overflow: hidden;
 

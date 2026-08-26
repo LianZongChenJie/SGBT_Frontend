3875e70b5df3f682ecae087f86d15f44c5bd957b
lfq <lufuqiang1225@163.com>
2026-08-26 11:45:31 +0800

    优化视频巡更历史

--- src/views/nengyuanzhan/anhuanguanli/shipinxungeng/lishi/DemoModal.vue
+++ src/views/nengyuanzhan/anhuanguanli/shipinxungeng/lishi/DemoModal.vue
@@ -1,11 +1,11 @@
 <template>
   <BasicModal v-bind="$attrs" @register="registerModal" :title="title" width="80%">
-    <a-tabs v-model:activeKey="activeKey">
+    <a-tabs v-model:activeKey="activeKey" class="record-tabs" @change="handleTabChange">
       <a-tab-pane key="1" tab="相机记录">
-        <BasicTable @register="registerTable"></BasicTable>
+        <BasicTable @register="registerTable" />
       </a-tab-pane>
-      <a-tab-pane key="2" tab="报警记录">
-        <BasicTable @register="registerTable2"></BasicTable>
+      <a-tab-pane key="2" tab="报警记录" force-render>
+        <BasicTable @register="registerTable2" />
       </a-tab-pane>
     </a-tabs>
   </BasicModal>
@@ -17,7 +17,7 @@
   import { getQueryAlarmRecords, getQueryCameraRecords } from './demo.api';
   import { columnsAlarm, columnsCamera } from './demo.data';
 
-  const taskId = ref(0);
+  const taskId = ref<string | number>();
   const taskName = ref('');
 
   const [registerModal, { setModalProps }] = useModalInner(async (data) => {
@@ -27,10 +27,11 @@
     isUpdate.value = !!data?.isUpdate;
     if (unref(isUpdate)) {
       taskId.value = data.record.id;
+      await Promise.all([reloadCameraRecords(), reloadAlarmRecords()]);
     }
   });
 
-  const [registerTable] = useTable({
+  const [registerTable, { reload: reloadCameraRecords }] = useTable({
     title: '',
     api: getQueryCameraRecords,
     columns: columnsCamera,
@@ -46,12 +47,14 @@
     tableSetting: { fullScreen: true },
     canResize: true,
     rowKey: 'id',
+    immediate: false,
+    pagination: false,
     beforeFetch: () => {
       return { taskId: taskId.value };
     },
   });
 
-  const [registerTable2] = useTable({
+  const [registerTable2, { reload: reloadAlarmRecords }] = useTable({
     title: '',
     api: getQueryAlarmRecords,
     columns: columnsAlarm,
@@ -67,10 +70,24 @@
     tableSetting: { fullScreen: true },
     canResize: false,
     rowKey: 'id',
+    immediate: false,
+    pagination: false,
     beforeFetch: () => {
       return { taskId: taskId.value };
     },
   });
+
+  async function handleTabChange(key: string) {
+    if (taskId.value == null) {
+      return;
+    }
+
+    if (key === '1') {
+      await reloadCameraRecords();
+    } else if (key === '2') {
+      await reloadAlarmRecords();
+    }
+  }
 
   const isUpdate = ref(true);
   const activeKey = ref('1');
@@ -85,3 +102,8 @@
     return taskName.value ? `${taskName.value} - 巡更任务详情` : '巡更任务详情';
   });
 </script>
+<style lang="less" scoped>
+  .record-tabs {
+    padding: 0 10px;
+  }
+</style>
